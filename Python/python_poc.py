800811f88d6deeb51d7ad47213aa690744dc788a
Connor Peoples <nouselesstech@connors-mbp.lan>
2022-09-24 22:42:35 -0500

    Added searches with regex to match long regex engine

--- Python/python_poc.py
+++ Python/python_poc.py
@@ -9,6 +9,7 @@ print("Doing the long regex method")
 print("Regex: /.*dolor.*elit.*do.*/")
 
 long_regex = re.compile(".*dolor.*elit.*do.*")
+short_regex = [re.compile("dolor"), re.compile("elit"), re.compile("do")]
 
 # Open the source text
 start_time = time.time()
@@ -18,6 +19,30 @@ with open('../lookup.txt') as source_file:
 end_time = time.time()
 
 print("Run time with long regex", int((end_time-start_time)*10000))
+
+print("\r\nDoing the individual, parallel lookup method with regex")
+
+# Open the source text
+start_time = time.time()
+with open('../lookup.txt') as source_file:
+    for line in source_file.readlines():
+        re.match(short_regex[0], line) and re.match(short_regex[1], line) and re.match(short_regex[2], line) 
+end_time = time.time()
+print("Run time with individual parallel lookup method with regex", int((end_time-start_time)*10000))
+
+print("\r\nDoing the individual, parallel lookup method")
+
+print("\r\nDoing the individual, serial lookup method with regex")
+
+# Open the source text
+start_time = time.time()
+with open('../lookup.txt') as source_file:
+    for line in source_file.readlines():
+        if re.match(short_regex[0], line):
+            if re.match(short_regex[1], line):
+                re.match(short_regex[2], line) 
+end_time = time.time()
+print("Run time with individual serial lookup method with regex", int((end_time-start_time)*10000))
 
 print("\r\nDoing the individual, parallel lookup method")
 
